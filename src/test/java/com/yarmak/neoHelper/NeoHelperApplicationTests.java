37b7d284f265a2715279c2839ae67971688e70b6
daraermak41@gmail.com <daraermak41@gmail.com>
2025-05-05 01:23:47 +0300

    first commit

---
+++ src/test/java/com/yarmak/neoHelper/NeoHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yarmak.neoHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
